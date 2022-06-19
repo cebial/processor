3ed1cfc69eb87e4be92470c21e669ff4d949cae1
cebial <biglazypanda@gmail.com>
2022-06-19 23:24:11 +0200

    Stage 2/6: Multiplication by number

--- Main.kt
+++ Main.kt
@@ -3,17 +3,14 @@ package processor
 fun readNums() = readln().split(" ").map { it.toInt() }
 
 fun main() {
-    val (rows1, cols1) = readNums()
-    val matrix1 = List(rows1) { readNums() }
+    val (rows, cols) = readNums()
+    val matrix = List(rows) { readNums() }
 
-    val (rows2, cols2) = readNums()
-    val matrix2 = List(rows2) { readNums() }
+    val multiplier = readln().toInt()
 
-    if (rows1 != rows2 || cols1 != cols2) { print("ERROR"); return }
-
-    for (y in 0 until rows1) {
-        for (x in 0 until cols1) {
-            print("${matrix1[y][x] + matrix2[y][x]} ")
+    for (y in 0 until rows) {
+        for (x in 0 until cols) {
+            print("${multiplier * matrix[y][x]} ")
         }
         println()
     }
