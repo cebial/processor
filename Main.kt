c8a5672b71d28c9564369f65ee7ed3fd636e3145
cebial <biglazypanda@gmail.com>
2022-06-22 15:00:37 +0200

    Stage 4/6: Transpose

--- Main.kt
+++ Main.kt
@@ -10,10 +10,6 @@ class Matrix(private val rows: Int, private val cols: Int, fromCli: Boolean = fa
 
     operator fun get(row: Int, col: Int) = matrix[row][col]
     operator fun set(row: Int, col: Int, value: Double) = matrix[row].set(col, value)
-    override fun toString() = matrix.joinToString("\n") { it.joinToString(" ") }
-
-    fun isEqualSize(other: Matrix) = rows == other.rows && cols == other.cols
-    fun canCompose(other: Matrix) = cols == other.rows
 
     operator fun plus(other: Matrix) = transform { x, y -> this[y, x] + other[y, x] }
     operator fun times(multiplier: Int) = transform { x, y -> multiplier * this[y, x] }
@@ -27,6 +23,11 @@ class Matrix(private val rows: Int, private val cols: Int, fromCli: Boolean = fa
         return newMatrix
     }
 
+    fun transposeMain() = transform { x, y -> this[x, y] }
+    fun transposeSide() = transform { x, y -> this[cols - 1 - x, rows - 1 - y] }
+    fun transposeVertical() = transform { x, y -> this[y, cols - 1 - x] }
+    fun transposeHorizontal() = transform { x, y -> this[rows - 1 - y, x] }
+
     private fun transform(operate: (x: Int, y: Int) -> Double): Matrix {
         val newMatrix = Matrix(rows, cols)
         for (row in 0 until rows)
@@ -35,10 +36,15 @@ class Matrix(private val rows: Int, private val cols: Int, fromCli: Boolean = fa
 
         return newMatrix
     }
+
+    fun isEqualSize(other: Matrix) = rows == other.rows && cols == other.cols
+    fun canCompose(other: Matrix) = cols == other.rows
+
+    override fun toString() = matrix.joinToString("\n") { it.joinToString(" ") }
  }
 
 class Processor {
-    private fun printResult(matrix: Matrix, msg: String = "The result is:") = println("$msg\n$matrix")
+    private fun printResult(matrix: Matrix) = println("The result is:\n$matrix")
 
     private fun readTwoMatrices() = readMatrix("first ") to readMatrix("second ")
     private fun readMatrix(prefix: String = ""): Matrix {
@@ -72,11 +78,35 @@ class Processor {
         printResult(matrix1 * matrix2)
     }
 
+    private fun transposeMatrix() {
+        print(
+            """ 
+            |1. Main diagonal
+            |2. Side diagonal
+            |3. Vertical line
+            |4. Horizontal line
+            |Your choice:
+            """.trimMargin()
+        )
+        val choice = readln().toInt()
+
+        val matrix = readMatrix()
+
+        printResult(when (choice) {
+            1 -> matrix.transposeMain()
+            2 -> matrix.transposeSide()
+            3 -> matrix.transposeVertical()
+            4 -> matrix.transposeHorizontal()
+            else -> error("Not a valid choice!")
+        })
+    }
+
     private fun showMenu() = print(
         """ 
         |1. Add matrices
         |2. Multiply matrix by a constant
         |3. Multiply matrices
+        |4. Transpose matrix
         |0. Exit
         |Your choice:
         """.trimMargin()
@@ -89,6 +119,7 @@ class Processor {
                 1 -> addMatrices()
                 2 -> scaleMatrix()
                 3 -> composeMatrices()
+                4 -> transposeMatrix()
                 else -> return
             }
             println()
